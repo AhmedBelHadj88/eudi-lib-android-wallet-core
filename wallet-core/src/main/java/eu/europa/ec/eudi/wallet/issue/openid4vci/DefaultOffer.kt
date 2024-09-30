125f8e01356cb2be64c53fc6712eb0bfc7e14d61
abelhadj <abelhadj@incert.lu>
2024-09-30 11:35:03 +0200

    minor change

--- wallet-core/src/main/java/eu/europa/ec/eudi/wallet/issue/openid4vci/DefaultOffer.kt
+++ wallet-core/src/main/java/eu/europa/ec/eudi/wallet/issue/openid4vci/DefaultOffer.kt
@@ -91,8 +91,8 @@ internal val CredentialConfiguration.name: String
  */
 internal val CredentialConfiguration.docType: String
     @JvmSynthetic get() = when (this) {
-        is MsoMdocCredential,
-        is SdJwtVcCredential -> docType
+        is MsoMdocCredential -> docType
+        is SdJwtVcCredential -> type
         else -> "unknown"
     }
 
